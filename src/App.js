131886c21ab49f350760504396c4ff665a60e345
Sergio Correa <stcf29@gmail.com>
2024-05-31 09:19:50 -0300

    feat:criação do site sem css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import AddressForm from './components/AddressForm';
+import EditAddress from './components/EditAddress';
+import Header from './components/Header/index';
 
-function App() {
+const App = () => {
+  const [addresses, setAddresses] = useState([]);
+  const [currentAddress, setCurrentAddress] = useState(null);
+
+  const handleAddAddress = (address) => {
+    setAddresses([...addresses, address]);
+  };
+
+  const handleEditAddress = (address) => {
+    setAddresses(
+      addresses.map((addr) => (addr === currentAddress ? address : addr))
+    );
+    setCurrentAddress(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='header'>
+        <Header>
+          <p>teste </p>
+        </Header>
+      </div>
+      <div className='menu'>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Cadastrar Endereço</Link>
+            </li>
+            <li>
+              <Link to="/edit">Editar Endereço</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <AddressForm onSubmit={handleAddAddress} />
+            <ul>
+              {addresses.map((address, index) => (
+                <li key={index}>
+                  {address.planet} - {address.code}
+                  <button onClick={() => setCurrentAddress(address)}>
+                    Editar
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </Route>
+          <Route path="/edit">
+            {currentAddress ? (
+              <EditAddress
+                initialAddress={currentAddress}
+                onSubmit={handleEditAddress}
+              />
+            ) : (
+              <p>Selecione um endereço para editar.</p>
+            )}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
